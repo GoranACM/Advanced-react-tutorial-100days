653fc565c5e299c66cf51dec4f197d14dd9e24b7
GoranACM <goranacm@gmail.com>
2020-10-27 18:42:18 +1100

    Simple counter

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,55 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+
+  const reset = () => {
+    setValue(0);
+  };
+
+  const complexIncrease = () => {
+    setTimeout(() => {
+      // setValue(value + 1);
+      setValue((prevState) => {
+        return prevState + 1;
+      });
+    }, 2000);
+  };
+
+  const complexDecrease = () => {
+    setTimeout(() => {
+      // setValue(value + 1);
+      setValue((prevState) => {
+        return prevState - 1;
+      });
+    }, 2000);
+  };
+
+  return (
+    <>
+      <section style={{ margin: '4rem 0' }}>
+        <h2>counter</h2>
+        <h1>{value}</h1>
+        <button className='btn' onClick={() => setValue(value - 1)}>
+          decrease
+        </button>
+        <button className='btn' onClick={reset}>
+          reset
+        </button>
+        <button className='btn' onClick={() => setValue(value + 1)}>
+          increase
+        </button>
+      </section>
+      <section style={{ margin: '4rem 0' }}>
+        <button className='btn' onClick={complexIncrease}>
+          increase in 2 seconds
+        </button>
+        <button className='btn' onClick={complexDecrease}>
+          decrease in 2 seconds
+        </button>
+      </section>
+    </>
+  );
 };
 
 export default UseStateCounter;

f0989445416830d331db0b75be4323b1a7a99a88
GoranACM <goranacm@gmail.com>
2020-10-30 21:57:54 +1100

    Adding items to a list on form submit

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -8,11 +8,23 @@ import React, { useState } from 'react';
 const ControlledInputs = () => {
   const [firstName, setFirstName] = useState('');
   const [email, setEmail] = useState('');
+  const [people, setPeople] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(firstName, email);
+    // console.log(firstName, email);
+    if (firstName && email) {
+      const person = { id: new Date().getTime().toString(), firstName, email };
+      setPeople((people) => {
+        return [...people, person];
+      });
+      setFirstName('');
+      setEmail('');
+    } else {
+      console.log('empty');
+    }
   };
+
   return (
     <>
       <article>
@@ -39,6 +51,15 @@ const ControlledInputs = () => {
           </div>
           <button type='submit'>Add person</button>
         </form>
+        {people.map((person, index) => {
+          const { id, firstName, email } = person;
+          return (
+            <div key={id} className='item'>
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </article>
     </>
   );
